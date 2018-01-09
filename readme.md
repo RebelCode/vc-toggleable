60915f93633d690a514719dc04b22bd6b93236ae
Roma Lytvynenko <litvinenko95@gmail.com>
2018-01-09 20:31:33 +0200

    readme documentation and code docs

--- readme.md
+++ readme.md
@@ -1,1 +1,35 @@
 # Toggleable Component
+
+## Usage
+```js
+<toggle :toggleable="toggleable" inline-template>
+    <span @click="setState(!isOn)">
+        {{ isOn ? 'On' : 'Off' }}
+    </span>
+</toggle>
+```
+Toggleable component implements logic responsible for getting Toggle model state and setting it. So consumer needs to use `inline-template`. Inside that template next functions and variables available: 
+
+`setState({boolean} newState)` - set toggle model state
+
+`isOn` - is toggle model state set to `true`
+
+## Developing
+Run `npm install` to install all dev dependencies.
+
+Here is available npm commands.
+
+Build library while developing
+```sh
+npm run dev
+```
+
+Build library for production
+```sh
+npm run production
+```
+
+Run e2e tests
+```sh
+npm run e2e
+```
--- src/index.js
+++ src/index.js
@@ -1,18 +1,48 @@
+/**
+ * Toggleable component.
+ *
+ * @since [*next-version*]
+ *
+ * @param Vue
+ * @return {*}
+ * @constructor
+ */
 export function CfToggleable (Vue) {
     return Vue.extend({
         props: {
+            /**
+             * Toggleable model.
+             *
+             * @since [*next-version*]
+             *
+             * @param {FunctionalToggleable}
+             */
             toggleable: {
                 required: true
             }
         },
 
         computed: {
+            /**
+             * Is current model value equals true.
+             *
+             * @since [*next-version*]
+             *
+             * @return {boolean}
+             */
             isOn () {
                 return this.toggleable.isOn()
             }
         },
 
         methods: {
+            /**
+             * Set new state value.
+             *
+             * @since [*next-version*]
+             *
+             * @param {boolean} state New state for toggleable model.
+             */
             setState (state) {
                 this.toggleable.setState(state)
             }
